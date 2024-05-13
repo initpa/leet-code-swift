662a3f54fc61c208db84b7a272a2ae9b1ebf66d5
initpa <>
2024-05-13 12:01:11 +0530

    array: contains duplicate

--- Arrays/ArraySolutions.playground/Contents.swift
+++ Arrays/ArraySolutions.playground/Contents.swift
@@ -1,6 +1,37 @@
 import UIKit
 
-print("hello world!")
+//MARK: - Contains duplicate
+/*
+ Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+ Example 1:
+
+ Input: nums = [1,2,3,1]
+ Output: true
+ Example 2:
+
+ Input: nums = [1,2,3,4]
+ Output: false
+ Example 3:
+
+ Input: nums = [1,1,1,3,3,4,3,2,4,2]
+ Output: true
+ */
+
+func containsDuplicate(_ nums: [Int]) -> Bool {
+    var seen = Set<Int>()
+    for value in nums {
+        if seen.contains(value) {
+            return true
+        }
+        seen.insert(value)
+    }
+    return false
+}
+
+print(containsDuplicate([1,2,3,1]))
+print(containsDuplicate([1,2,3,4]))
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+
 
 
 
