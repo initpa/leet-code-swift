851703373a30517b9844b41582d463222053dedf
initpa <nitpa.bh@gmail.com>
2024-05-14 11:58:11 +0530

    move non zeros

--- Arrays/ArraySolutions.playground/Contents.swift
+++ Arrays/ArraySolutions.playground/Contents.swift
@@ -28,9 +28,48 @@ func containsDuplicate(_ nums: [Int]) -> Bool {
     return false
 }
 
-print(containsDuplicate([1,2,3,1]))
-print(containsDuplicate([1,2,3,4]))
-print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+//print(containsDuplicate([1,2,3,1]))
+//print(containsDuplicate([1,2,3,4]))
+//print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
+
+
+// MARK: 283. Move non zeros, Easy
+/*
+ Array, Two pointers
+ Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ Note that you must do this in-place without making a copy of the array.
+ */
+
+func moveZeroes(_ nums: inout [Int]) {
+    var nonZeroIndex: Int = 0
+    for i in 0..<nums.count {
+        if nums[i] != 0 {
+            nums.swapAt(i, nonZeroIndex)
+            nonZeroIndex += 1
+        }
+    }
+}
+
+//func moveZeroes(_ nums: inout [Int]) {
+//    var incrementIndex: Int = 0
+//
+//    for i in 0..<nums.count {
+//        if nums[i] != 0 {
+//            nums[incrementIndex] = nums[i]
+//            incrementIndex += 1
+//        }
+//    }
+//
+//    for i in incrementIndex..<nums.count {
+//        nums[i] = 0
+//    }
+//}
+
+//var nums = [0,1,0,3,12]
+//moveZeroes(&nums)
+//print(nums)
+
+
 
 
 
