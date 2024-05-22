15886d03ade9b892a59666c5887c0f4fa9e8d837
initpa <nitpa.bh@gmail.com>
2024-05-22 15:46:38 +0530

    88. Merge sorted array.

--- Arrays/ArraySolutions.playground/Contents.swift
+++ Arrays/ArraySolutions.playground/Contents.swift
@@ -1,5 +1,50 @@
 import UIKit
 
+//MARK: - Easy
+//MARK: - 88. Merge sorted array.
+
+
+func mergeSortedArrays(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+    var index1 = m - 1
+    var index2 = n - 1
+    var mergedIndex = m + n - 1
+    
+    while index1 >= 0 && index2 >= 0 {
+        if nums1[index1] > nums2[index2] {
+            nums1[mergedIndex] = nums1[index1]
+            index1 -= 1
+        } else {
+            nums1[mergedIndex] = nums2[index2]
+            index2 -= 1
+        }
+        mergedIndex -= 1
+    }
+    
+    while index2 >= 0 {
+        nums1[mergedIndex] = nums2[index2]
+        index2 -= 1
+        mergedIndex -= 1
+    }
+}
+
+// Example usage:
+//var nums1 = [1,2,3,0,0,0]
+//let m = 3
+//let nums2 = [2,5,6]
+//let n = 3
+//
+//mergeSortedArrays(&nums1, m, nums2, n)
+//print(nums1)
+
+var nums1 = [1,2,3,0,0,0]
+let m = 3
+let nums2 = [2,5,6]
+let n = 3
+
+mergeSortedArrays(&nums1, m, nums1, n)
+print(nums1)
+
+
 //MARK: - 217. Contains duplicate, Easy
 /*
  Arrays
