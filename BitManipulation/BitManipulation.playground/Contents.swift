0c9dab62b5e168868255be6d8b0ee8cd53459808
initpa <nitpa.bh@gmail.com>
2024-05-26 12:36:31 +0530

    missing number question 1

--- BitManipulation/BitManipulation.playground/Contents.swift
+++ BitManipulation/BitManipulation.playground/Contents.swift
@@ -2,3 +2,20 @@ import UIKit
 import PlaygroundSupport
 import Foundation
 
+// EASY
+// MARK: - 268. Missing number
+/*
+ Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+ */
+
+func missingNumber(_ nums: [Int]) -> Int {
+    let n = nums.count
+    let currentSum = nums.reduce(0, +)
+    let totalSum = n*(n+1)/2
+    
+    return totalSum - currentSum
+}
+
+print(missingNumber([3,0,1]))
+
+
