b20e55fdde09a793b42981039d7e5698f3bf61d3
initpa <nitpa.bh@gmail.com>
2024-05-26 12:50:41 +0530

    268. missing number XOR

--- BitManipulation/BitManipulation.playground/Contents.swift
+++ BitManipulation/BitManipulation.playground/Contents.swift
@@ -8,12 +8,21 @@ import Foundation
  Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
  */
 
+//func missingNumber(_ nums: [Int]) -> Int {
+//    let n = nums.count
+//    let currentSum = nums.reduce(0, +)
+//    let totalSum = n*(n+1)/2
+//    
+//    return totalSum - currentSum
+//}
+
+// bit manipulation XOR
 func missingNumber(_ nums: [Int]) -> Int {
-    let n = nums.count
-    let currentSum = nums.reduce(0, +)
-    let totalSum = n*(n+1)/2
-    
-    return totalSum - currentSum
+    var missingNumber = 0
+    for i in 0..<nums.count {
+        missingNumber ^= (i+1) ^ nums[i]
+    }
+    return missingNumber
 }
 
 print(missingNumber([3,0,1]))
