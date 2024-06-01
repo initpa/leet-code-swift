1b9131d723bd864cff1f8b515b0dd1c1311aae06
initpa <nitpa.bh@gmail.com>
2024-06-01 21:05:43 +0530

    153. find minimum in rotated array

--- BinarySearch/BinarySearch.playground/Contents.swift
+++ BinarySearch/BinarySearch.playground/Contents.swift
@@ -90,3 +90,38 @@ func findDuplicate(_ nums: [Int]) -> Int {
     return slow
 }
 print(findDuplicate([1,3,4,2,2]))
+
+
+// MARK: - 153. find minimum in rotated array
+/*
+ Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, the array nums = [0,1,2,4,5,6,7] might become:
+
+ [4,5,6,7,0,1,2] if it was rotated 4 times.
+ [0,1,2,4,5,6,7] if it was rotated 7 times.
+ Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+ Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+ You must write an algorithm that runs in O(log n) time.
+ */
+
+
+func findMin(_ nums: [Int]) -> Int {
+    var left = 0
+    var right = nums.count - 1
+    
+    while left < right {
+        let mid = left + (right - left) / 2
+        
+        if nums[mid] > nums[right] {
+            left = mid + 1
+        } else {
+            right = mid
+        }
+    }
+    
+    return nums[left]
+}
+
+
+print(findMin([3,4,5,1,2]))
