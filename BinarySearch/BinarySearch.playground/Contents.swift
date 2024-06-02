0f1b335482a445e998ca988342ad1d74ca2bdb96
initpa <nitpa.bh@gmail.com>
2024-06-02 21:22:39 +0530

    74. search 2D matrix

--- BinarySearch/BinarySearch.playground/Contents.swift
+++ BinarySearch/BinarySearch.playground/Contents.swift
@@ -124,4 +124,36 @@ func findMin(_ nums: [Int]) -> Int {
 }
 
 
-print(findMin([3,4,5,1,2]))
+//print(findMin([3,4,5,1,2]))
+
+//MARK: - 74. search 2D matrix
+
+/*
+ You are given an m x n integer matrix matrix with the following two properties:
+ 
+ Each row is sorted in non-decreasing order.
+ The first integer of each row is greater than the last integer of the previous row.
+ Given an integer target, return true if target is in matrix or false otherwise.
+
+ You must write a solution in O(log(m * n)) time complexity.
+ */
+
+func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+    for row in matrix {
+        for num in row {
+            if num == target {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+let matrix = [
+    [1, 3, 5, 7],
+    [10, 11, 16, 20],
+    [23, 30, 34, 60]
+]
+let target = 3
+
+print(searchMatrix(matrix, target))
